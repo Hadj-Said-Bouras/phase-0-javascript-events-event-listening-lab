265f67547d11cae5fb07938ca1822b62921b40f4
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-08-04 17:34:00 +0100

    Done with the assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+const seinput = document.getElementById('button');
+
+seinput.addEventListener('click', function click() {
+    alert('I was clicked') })
 }
+
+addingEventListener()
